2747c328ebdccda4b598e9f8d8fec4cf8cb7d92a
Aleksiej Ostrowski <aleksiej.ostrowski@gmail.com>
2022-02-23 15:38:02 +0300

    add time in step3

--- step3.sh
+++ step3.sh
@@ -1,5 +1,5 @@
 echo "=== step 3 ==="
 
-ffmpeg -pattern_type glob -i "./frames3/*.png" 3.webm
+time ffmpeg -pattern_type glob -i "./frames3/*.png" 3.webm
 # ffmpeg -pattern_type glob -i "./frames3/*.png" -i "./audio1/audio.ogg" 3.webm
 md5sum 3.webm
